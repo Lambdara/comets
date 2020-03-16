9801d7a54f87ae64ee2617eea4ff310ea1291363
Uwila <uwila@runbox.com>
2020-03-16 14:18:03 +0100

    Fix shader file extension

--- graphics.h
+++ graphics.h
@@ -1,8 +1,8 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
-#define VERTEX_SHADER_PATH "vertexshader.vs"
-#define FRAGMENT_SHADER_PATH "fragmentshader.fs"
+#define VERTEX_SHADER_PATH "vertexshader.glsl"
+#define FRAGMENT_SHADER_PATH "fragmentshader.glsl"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
