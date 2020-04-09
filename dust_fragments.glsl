4b5d52aee626739f381a46bba536d5c4e85ba74a
Uwila <uwila@runbox.com>
2020-04-09 17:42:00 +0200

    Better lighting on dust

--- dust_fragments.glsl
+++ dust_fragments.glsl
@@ -1,8 +1,21 @@
 #version 330 core
 
+in vec3 fragment_position;
+
 out vec4 pixel_color;
 
 void main()
 {
-    pixel_color = vec4(1.0, 1.0, 1.0, 1.0);
+    float diffuse_light = 1.0;
+    float ambient_light = 0.025;
+    float light = diffuse_light + ambient_light;
+    float dist = (1000-length(fragment_position))/1000;
+
+    light = light * dist * dist;
+
+    if (length(fragment_position) > 800.0) {
+        light = light * (1000 - length(fragment_position)) / 200.0;
+    }
+
+    pixel_color = light*vec4(1.0, 1.0, 1.0, 1.0);
 }
--- dust_vertices.glsl
+++ dust_vertices.glsl
@@ -4,7 +4,10 @@ layout (location = 0) in vec3 position;
 uniform mat4 view_matrix;
 uniform mat4 projection_matrix;
 
+out vec3 fragment_position;
+
 void main()
 {
     gl_Position = projection_matrix * view_matrix * vec4(position, 0.1);
+    fragment_position = position;
 }
